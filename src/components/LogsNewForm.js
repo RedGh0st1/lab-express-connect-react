3e071c391552c7a9af25a6bbacc68ec279de4790
Lennie Nurse <lnursej@gmail.com>
2023-01-16 16:55:01 -0500

    "update"

--- src/components/LogsNewForm.js
+++ src/components/LogsNewForm.js
@@ -1,4 +1,8 @@
 import React from "react"
+import { useEffect } from "react"
+import axios from "axios"
+
+const API = process.env.REACT_APP_API_URL
 
 export default function LogsNewForm() {
   return <div>LogsNewForm</div>
