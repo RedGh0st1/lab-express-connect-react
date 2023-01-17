d4a58207dbfa176ef7be3ec6e61840441620723e
Lennie Nurse <lnursej@gmail.com>
2023-01-17 08:15:41 -0500

    "updated"

--- src/components/LogsNewForm.js
+++ src/components/LogsNewForm.js
@@ -7,10 +7,19 @@ const API = process.env.REACT_APP_API_URL
 
 export default function LogsNewForm() {
   const navigate = useNavigate()
-  const [log, setNewLog] = useState({})
+  const [log, setNewLog] = useState({
+    captainName: "",
+    title: "",
+    mistakesWereMadeToday: false,
+    daysSinceLastCrisis: 0,
+  })
 
   function handleTextChange(event) {
     setNewLog({ ...log, [event.target.id]: event.target.value })
+  }
+
+  function handleCheckbox(event) {
+    setNewLog({ ...log, mistakesWereMadeToday: !log.mistakesWereMadeToday })
   }
 
   function handleSubmit(event) {
@@ -26,16 +35,49 @@ export default function LogsNewForm() {
       <Form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input
-          id="name"
-          value={log.name}
+          id="captainName"
+          value={log.captainName}
           type="text"
-          placeholder="Name of "
+          placeholder="Name of captain"
           onChange={handleTextChange}
+          required
         />
-        <label htmlFor=""></label>
-        <input />
-        <label htmlFor=""></label>
-        <input />
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          value={log.title}
+          type="text"
+          placeholder="title"
+          onChange={handleTextChange}
+          required
+        />
+        <label htmlFor="posts">Post:</label>
+        <input
+          id="post"
+          value={log.post}
+          placeholder="post"
+          onChange={handleTextChange}
+          type="text"
+          required
+        />
+        <label htmlFor="mistakesWereMadeToday">Mistakes were made Today:</label>
+        <input
+          id="mistakesWereMadeToday"
+          value={log.mistakesWereMadeToday}
+          type="checkbox"
+          onChange={handleCheckbox}
+          required
+        />
+        <label htmlFor="daysSinceLastCrisis">Days since last Crisis:</label>
+        <input
+          id="daysSinceLastCrisis"
+          value={log.daysSinceLastCrisis}
+          type="text"
+          placeholder="last crisis"
+          onChange={handleTextChange}
+          required
+        />
+        <input type="submit" />
       </Form>
     </div>
   )
